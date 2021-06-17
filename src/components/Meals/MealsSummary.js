57d3a8024447e85a1cd4a3a406854385939e76d8
ErickCruz3 <30erickC@gmail.com>
2021-06-17 11:41:04 -0500

    Adición de un componente "Comidas"

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Comida Deliciosa, Entregada a Ti</h2>
+        <p>
+           Elija su Comida Favorita de Nuestra Amplia Selección de Comidas Disponibles
+           y Disfruta de un Delicioso Almuerzo o Cena en Casa.
+        </p>
+        <p>
+           Todas Nuestras Comidas se Cocinan con Ingredientes de Alta Calidad, Justo a Tiempo y
+           ¡Por Supuesto por Chefs Experimentados!
+        </p>
+    </section>
+}
+
+export default MealsSummary
