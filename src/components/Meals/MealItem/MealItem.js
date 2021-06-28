ea72054b3be749fcab452e9a77f997f375561671
ErickCruz3 <30erickC@gmail.com>
2021-06-28 08:57:22 -0500

    Adicion de componente "comidas" 2

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,14 @@
+const MealItem = (props) => {
+    return(
+        <li>
+            <div>
+                <h3>{props.name}</h3>
+                <div>{props.description}</div>
+                <div>{props.price}</div>
+            </div>
+            <div></div>
+        </li>
+    )
+}
+
+export default MealItem
