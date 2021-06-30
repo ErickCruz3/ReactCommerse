95d984905d0dfb08efbe88b32f3f0ea9e86d6c77
ErickCruz3 <30erickC@gmail.com>
2021-06-30 08:42:02 -0500

    Agregar Un Context al Carrito

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import Header from './components/Layout/Header'
 import Meals from "./components/Meals/Meals"
 import Cart from "./components/Card/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShow, setCartIsShow] = useState()
@@ -16,13 +17,13 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShow && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
