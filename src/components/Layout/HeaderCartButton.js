2eb529d9208694fc491afddc379ed305a31caa38
ErickCruz3 <30erickC@gmail.com>
2021-06-30 09:15:02 -0500

    Uso del Contex en carrito

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,22 @@
+import { useContext } from 'react'
+
 import CartIcon from '../Card/CartIcon'
+import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext)
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amunt
+    }, 0)
+
     return <button className={classes.button} onClick={props.onClick} >
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Tu Carrito</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 }
 
