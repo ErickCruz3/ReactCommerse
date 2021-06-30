7798c1b8d5083577f014611f677aa5a13e2ef8c8
ErickCruz3 <30erickC@gmail.com>
2021-06-30 11:30:16 -0500

    Uso de Referencias y referencias posteriores en el Carrito

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,23 +1,23 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 
-import CartIcon from '../Card/CartIcon'
-import CartContext from '../../store/cart-context'
-import classes from './HeaderCartButton.module.css'
+import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton = props => {
-    const cartCtx = useContext(CartContext)
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amunt
-    }, 0)
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    return <button className={classes.button} onClick={props.onClick} >
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+      </span>
         <span>Tu Carrito</span>
         <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  )
 }
 
 export default HeaderCartButton
